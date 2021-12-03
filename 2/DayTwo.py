c99dd815798f8fa22f98e4b8444e21bddf7e4706
Anders Dehlbom <anders.dehlbom@gmail.com>
2021-12-03 18:49:43 +0100

    finished

--- 2/DayTwo.py
+++ 2/DayTwo.py
@@ -21,9 +21,35 @@ def part_one():
                         up.append(int(value[1]))
                     case "down":
                         down.append(int(value[1]))
+    f.close()
     return sum(forward)*(sum(down)-sum(up))
-print(part_one())
+print("part 1 depth: " + str(part_one()))
     
+def part_two():
+    with open('./2/input.txt') as f:
+            list = []
+            horizontal_pos = 0
+            depth = 0
+            aim = 0
+            for line in f:
+                list.append(line.rstrip().split(" "))
+                
+            for value in list:
+                
+                for item in value:
+                    match item:
+                        case "forward":
+                            horizontal_pos+=int(value[1])
+                            depth+=int(value[1])*aim
+                        case "up":
+                            aim-=int(value[1])
+                        case "down":
+                            aim+=int(value[1])
+    f.close()
+    return horizontal_pos*depth
+
+
+print("part 2 depth: " + str(part_two()))        
 
                     
 
