e4bc3a44a3638e207130fc2134dbfc3440ac897f
Anders Dehlbom <anders.dehlbom@gmail.com>
2021-12-03 17:44:53 +0100

    Update DayOne.py

--- 1/DayOne.py
+++ 1/DayOne.py
@@ -23,7 +23,7 @@ def sumThreeIndices():
         for line in f:
            list.append(line.rstrip())
         
-        while(len(list)>=6):
+        while(len(list)%6):
             sum1 = int(list[0]) + int(list[1]) + int(list[2])
             sum2 = int(list[1]) + int(list[2]) + int(list[3])
 
