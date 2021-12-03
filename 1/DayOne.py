82cd63be44c672545766181721cfe79a65b16295
Anders Dehlbom <anders.dehlbom@gmail.com>
2021-12-03 17:43:15 +0100

    Update DayOne.py

--- 1/DayOne.py
+++ 1/DayOne.py
@@ -12,14 +12,32 @@ def countDepth():
                 c+=1
             currentNumber = l
             
-
+    f.close()
     return c
 
+def sumThreeIndices():
+    with open('./1/input.txt') as f:
+        list = []
+        c = 0
 
-print(countDepth())
+        for line in f:
+           list.append(line.rstrip())
         
+        while(len(list)>=6):
+            sum1 = int(list[0]) + int(list[1]) + int(list[2])
+            sum2 = int(list[1]) + int(list[2]) + int(list[3])
 
-        
+            if(sum2>sum1):
+                c+=1
+            
+           
+            list.pop(0)
+
+
+        f.close()
+        return c  
+
+print(sumThreeIndices())
 
 
 
